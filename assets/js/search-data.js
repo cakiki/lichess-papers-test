f7b45bc19bdae1dfbfbe353d3d3ba7b37ed80d33
cakiki <christopher.akiki@gmail.com>
2025-09-08 14:38:42 +0000

    Deploying to gh-pages from @ cakiki/lichess-papers-test@22ffb2107bcd5950edc4ef5ae667fc7a7d1d8b72 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/lichess-papers-test/repositories/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/lichess-papers-test/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/lichess-papers-test/blog/";
-              },
-            },{id: "post-a-post-with-plotly-js",
+        },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
         
